19e1e7b988255b66ee1c034b6caf2f72ef629c3a
privef <privef@krakenator.imag.fr>
2018-12-22 21:59:44 +0100

    save

--- 1-get-height.R
+++ 1-get-height.R
@@ -71,14 +71,14 @@ times <- rep(NA, 4)
 preds <- matrix(NA, length(ind.test), 4)
 for (i in 1:4) {
   times[i] <- system.time(
-    logit <- big_spLinReg(G, y[sub][ind.train], ind.train,
-                          ind.col = which(CHR %in% chrs[[i]]),
-                          covar.train = PC[sub[ind.train], ],
-                          base.train = pred.base[ind.train],
-                          ncores = 10)
+    mod <- big_spLinReg(G, y[sub][ind.train], ind.train,
+                        ind.col = which(CHR %in% chrs[[i]]),
+                        covar.train = PC[sub[ind.train], ],
+                        base.train = pred.base[ind.train],
+                        ncores = 10)
   )[3]
   preds[, i] <- pred.base[ind.test] +
-    predict(logit, G, ind.test, covar.row = PC[sub[ind.test], ])
+    predict(mod, G, ind.test, covar.row = PC[sub[ind.test], ])
 }
 
 library(dplyr)
@@ -104,16 +104,46 @@ res
 # 8   Male 0.6551584 656060 75378.401
 
 library(ggplot2)
-ggplot(res) +
+ggplot(res, aes(nvar, time / 3600)) +
   theme_bigstatsr() +
-  geom_point(aes(nvar, cor, color = sex), size = 3) +
-  geom_line(aes(nvar, cor, color = sex), size = 1.5) +
-  labs(x = "Number of variables (350K individuals)",
-       y = "Correlation between predicted and true heights",
-       color = "Sex") +
-  theme(legend.position = c(0.2, 0.85)) +
-  geom_point(aes(nvar, time / 3600 / 40), size = 3) +
-  geom_line(aes(nvar, time / 3600 / 40), size = 1.5) +
-  scale_y_continuous(sec.axis = sec_axis(~.*40, name = "Fitting time (in hours)"))
+  geom_point(size = 3) +
+  geom_line(size = 1.5) +
+    labs(x = "Number of variables (350K individuals)",
+         y = "Fitting time (in hours)")
 
 ggsave("height-paper2.pdf", width = 887, height = 755, scale = 1/100)
+
+
+# C+T
+COVAR <- cbind(PC, df0$date, df0$sex)
+system.time(
+  res2 <- pkg.paper.PRS::PRS(G, CHR, ukb$map$physical.pos, y[sub], COVAR[sub, ],
+                             ind.train, ind.test, family = "gaussian")
+) # 69 min for the GWAS part
+
+cor_by_sex <- lapply(res2$pred, function(pred) {
+  # should have been learned on training set, but let's be optimistic
+  mylm <- lm(y ~ pred + COVAR, data.frame(pred, y = y[sub][ind.test],
+                                          COVAR = I(COVAR[sub[ind.test], ])))
+  pred2 <- predict(mylm)
+  tapply(seq_along(pred2), df0$sex[sub[ind.test]], function(ind) {
+    cor(pred2[ind], y[sub][ind.test][ind])
+  })
+})
+
+res2$cor_female <- purrr::map_dbl(cor_by_sex, "Female")
+res2$cor_male   <- purrr::map_dbl(cor_by_sex, "Male")
+saveRDS(res2, "height-paper2-gwas.rds")
+res2
+# # A tibble: 9 x 7
+#   method        pred          thr.r2 set           timing cor_female cor_male
+#   <chr>         <list>         <dbl> <list>         <dbl>      <dbl>    <dbl>
+# 1 PRS-all       <dbl [24,131…   0.05 <int [158,38…  4148.      0.528    0.537
+# 2 PRS-stringent <dbl [24,131…   0.05 <int [692]>    4148.      0.454    0.457
+# 3 PRS-max       <dbl [24,131…   0.05 <int [13,715…  4148.      0.551    0.557
+# 4 PRS-all       <dbl [24,131…   0.2  <int [292,93…  4148.      0.537    0.551
+# 5 PRS-stringent <dbl [24,131…   0.2  <int [1,055]>  4148.      0.426    0.429
+# 6 PRS-max       <dbl [24,131…   0.2  <int [45,570…  4148.      0.549    0.561
+# 7 PRS-all       <dbl [24,131…   0.8  <int [575,15…  4148.      0.484    0.494
+# 8 PRS-stringent <dbl [24,131…   0.8  <int [2,635]>  4148.      0.327    0.327
+# 9 PRS-max       <dbl [24,131…   0.8  <int [575,15…  4148.      0.484    0.494
---
+++ 2-get-BC.R
@@ -0,0 +1,98 @@
+library(doParallel)
+registerDoParallel(cl <- makeCluster(12))
+system.time({
+  list_snp_id <- foreach(chr = 1:22) %dopar% {
+    # cat("Processing chromosome", chr, "..\n")
+    mfi <- paste0("data/ukb_imp_mfi/ukb_mfi_chr", chr, "_v3.txt")
+    infos_chr <- bigreadr::fread2(mfi, showProgress = FALSE)
+    infos_chr_sub <- subset(infos_chr, V6 > 0.01)  ## MAF > 1%
+    bim <- paste0("data/ukb_snp_bim/ukb_snp_chr", chr, "_v2.bim")
+    map_chr <- bigreadr::fread2(bim)
+    joined <- dplyr::inner_join(
+      infos_chr_sub, map_chr,
+      by = c("V3" = "V4", "V4" = "V5", "V5" = "V6")
+    )
+    with(joined, paste(chr, V3, V4, V5, sep = "_"))
+  }
+}) # 80 sec
+stopCluster(cl)
+
+sum(lengths(list_snp_id))  # 656,060
+
+sample <- bigreadr::fread2("ukb25589_imp_chr1_v3_s487327.sample")
+str(sample)
+(N <- readBin("data/ukb_imp_chr1_v3.bgen", what = 1L, size = 4, n = 4)[4])
+sample <- sample[-1, ]
+stopifnot(nrow(sample) == N)
+
+df0 <- readRDS("pheno.rds")
+ind.indiv <- match(df0$eid, sample$ID_2)
+y <- df0$has_cancer
+y[y] <- NA  ## set to missing all types of cancer
+y[df0$cancer_type == "breast cancer"] <- 1  ## keep only BC
+
+sub <- which(df0$sex == "Female" & df0$is_caucasian & !is.na(y) & !is.na(ind.indiv))
+length(sub)  # 190,039
+table(y[sub])
+#      0      1
+# 181936   8103
+
+
+system.time(
+  rds <- bigsnpr::snp_readBGEN(
+    bgenfiles = glue::glue("data/ukb_imp_chr{chr}_v3.bgen", chr = 1:22),
+    list_snp_id = list_snp_id,
+    backingfile = "data/UKB_imp_BC",
+    ind_row = ind.indiv[sub],
+    bgi_dir = "data/ukb_imp_bgi",
+    ncores = 10
+  )
+) # 2H
+
+
+set.seed(1)
+ind.train <- sort(sample(length(sub), 150e3))
+ind.test <- setdiff(seq_along(sub), ind.train)
+table(y[sub][ind.train])
+#      0      1
+# 143626   6374
+
+library(bigsnpr)
+ukb <- snp_attach("data/UKB_imp_BC.rds")
+G <- ukb$genotypes
+CHR <- as.numeric(ukb$map$chromosome)
+dim(G) # 190,039 x 656,060
+file.size(G$backingfile) / 1024^3  # 116 GB
+
+PC <- as.matrix(readRDS("PC.rds"))
+plot(PC[sample(nrow(PC), 10e3), ], pch = 20)  # verif: all
+plot(PC[sample(sub, 10e3), ], pch = 20)       # verif: caucasian only
+
+system.time(
+  mod <- big_spLogReg(G, y[sub][ind.train], ind.train,
+                      covar.train = PC[sub[ind.train], ],
+                      ncores = 10)
+) # 12 min
+sum(rowMeans(sapply(mod, function(x) x$beta.X)) > 0) # 397
+pred <- predict(mod, G, ind.test, covar.row = PC[sub[ind.test], ])
+AUC(pred, y[sub][ind.test])  # 58.7
+
+# C+T
+res <- pkg.paper.PRS::PRS(G, CHR, ukb$map$physical.pos, y[sub], PC[sub, ],
+                          ind.train, ind.test)
+# 15h for the GWAS part
+res$AUC <- sapply(res$pred, function(pred) AUC(pred, y[sub[ind.test]]))
+saveRDS(res, "BC-paper2-gwas.rds")
+res
+# # A tibble: 9 x 6
+#   method        pred           thr.r2 set             timing   AUC
+#   <chr>         <list>          <dbl> <list>           <dbl> <dbl>
+# 1 PRS-all       <dbl [40,039]>   0.05 <int [159,391]> 55444. 0.537
+# 2 PRS-stringent <dbl [40,039]>   0.05 <int [7]>       55444. 0.578
+# 3 PRS-max       <dbl [40,039]>   0.05 <int [24]>      55444. 0.586
+# 4 PRS-all       <dbl [40,039]>   0.2  <int [293,211]> 55444. 0.543
+# 5 PRS-stringent <dbl [40,039]>   0.2  <int [8]>       55444. 0.573
+# 6 PRS-max       <dbl [40,039]>   0.2  <int [31]>      55444. 0.587
+# 7 PRS-all       <dbl [40,039]>   0.8  <int [575,240]> 55444. 0.550
+# 8 PRS-stringent <dbl [40,039]>   0.8  <int [13]>      55444. 0.564
+# 9 PRS-max       <dbl [40,039]>   0.8  <int [114]>     55444. 0.583
