7db2df22835d806c177b00cdd460919c3d0c113c
privef <privef@krakenator.imag.fr>
2019-05-01 17:40:02 +0200

    save CAD / Asthma

--- 08-SCT-asthma.R
+++ 08-SCT-asthma.R
@@ -4,48 +4,33 @@ unzip("sumstats_asthma.zip")
 library(bigreadr)
 sumstats <- fread2(
   "TAGC meta-analyses results for asthma risk/TAGC_Multiancestry_and_European-Ancestry_Meta-analyses_Results.tsv",
-  select = c(1, 3:5, 18, 20), col.names = c("chr", "pos", "a0", "a1", "beta", "p")
+  select = c(1, 3:5, 18, 20),
+  col.names = c("chr", "pos", "a0", "a1", "beta", "p")
 )
 nrow(sumstats)  # 2,001,280
+
+sumstats <- subset(sumstats, p < 0.5)
+nrow(sumstats)  # 1,013,806
 hist(sumstats$beta)
 hist(sumstats$p)
 
-# augment dataset to match reverse alleles
-sumstats2 <- sumstats
-sumstats2$a0 <- sumstats$a1
-sumstats2$a1 <- sumstats$a0
-sumstats2$beta <- -sumstats$beta
-sumstats2 <- rbind(sumstats, sumstats2)
+info_snp_UKBB <- rbind_df(lapply(1:22, function(chr) {
+  file <- paste0("data/ukb_imp_mfi/ukb_mfi_chr", chr, "_v3.txt")
+  df <- fread2(file, select = c(3:5, 8), col.names = c("pos", "a0", "a1", "info"))
+  cbind.data.frame(chr = chr, df)
+}))
+info_snp <- bigsnpr::snp_match(sumstats, info_snp_UKBB)
+# 1,013,806 variants in summary statistics.
+# 0 ambiguous SNPs have been removed.
+# 1,013,119 variants have been matched; 0 were flipped and 1 were reversed.
+info_snp <- subset(na.omit(info_snp), info > 0.3)
 
-# match variants with UKBB
-library(doParallel)
-NCORES <- 12
-registerDoParallel(cl <- makeCluster(NCORES))
-info_snp <- foreach(chr = 1:22, .packages = "dplyr") %dopar% {
+list_snp_id <- with(info_snp, split(paste(chr, pos, a0, a1, sep = "_"),
+                                    factor(chr, levels = 1:22)))
+beta <- info_snp$beta
+lpval <- -log10(info_snp$p)
+info <- info_snp$info
 
-  paste0("data/ukb_imp_mfi/ukb_mfi_chr", chr, "_v3.txt") %>%
-    bigreadr::fread2() %>%
-    inner_join(
-      sumstats2[sumstats2$chr == chr, ], ., na_matches = "never",
-      by = c(pos = "V3", a0 = "V4", a1 = "V5")
-    ) %>%
-    arrange(pos) %>%
-    transmute(
-      id    = paste(chr, pos, a0, a1, sep = "_"),
-      beta  = beta,
-      lpval = -log10(p),
-      info  = V8
-    ) %>%
-    na.omit() %>%
-    filter(lpval > 1 & info > 0.3)
-}
-stopCluster(cl)
-
-list_snp_id <-  lapply(info_snp, function(df) df$id)
-beta <-  unlist(lapply(info_snp, function(df) df$beta))
-lpval <- unlist(lapply(info_snp, function(df) df$lpval))
-info <-  unlist(lapply(info_snp, function(df) df$info))
-length(beta)  # 218,778
 
 # subset samples
 sample <- fread2("ukb25589_imp_chr1_v3_s487327.sample")
@@ -88,6 +73,8 @@ table(y.sub <- y[sub])
 #      0      1
 # 261985  43787
 
+
+NCORES <- 12
 system.time(
   rds <- bigsnpr::snp_readBGEN(
     bgenfiles = glue::glue("data/ukb_imp_chr{chr}_v3.bgen", chr = 1:22),
@@ -97,13 +84,13 @@ system.time(
     bgi_dir = "data/ukb_imp_bgi",
     ncores = NCORES
   )
-) # 1H
+) # 3.8H
 
 
 library(bigsnpr)
 ukbb <- snp_attach("data/UKBB_asthma.rds")
 G <- ukbb$genotypes
-file.size(G$backingfile) / 1024^3  # 62 GB
+file.size(G$backingfile) / 1024^3  # 288 GB
 CHR <- as.integer(ukbb$map$chromosome)
 POS <- ukbb$map$physical.pos
 
@@ -119,53 +106,48 @@ system.time(
     grid.base.size = c(50, 100, 200, 500),
     ncores = NCORES
   )
-) # 12 min
-plot(lengths(all_keep[[1]]), pch = 20)
+) # 4.3H
 
 system.time(
   multi_PRS <- snp_grid_PRS(
     G, all_keep, betas = beta, lpS = lpval, ind.row = ind.train,
-    grid.lpS.thr = seq_log(1, 0.999 * max(lpval), 50),
-    backingfile = "data/UKBB_asthma_scores", ncores = NCORES
+    n_thr_lpS = 50, backingfile = "data/UKBB_asthma_scores", ncores = NCORES
   )
-) # 13 min
+) # 1.7H
 
 system.time(
   final_mod <- snp_grid_stacking(
     multi_PRS, y.sub[ind.train], ncores = NCORES, n.abort = 3
   )
-) # 14H
+) # 12H
 mod <- final_mod$mod
 plot(mod)
 summary(mod)
-# # A tibble: 5 x 6
-#      alpha validation_loss intercept beta           nb_var message
-#      <dbl>           <dbl>     <dbl> <list>          <int> <list>
-#   1 0.0001           0.403     -1.24 <dbl [77,280]>  60782 <chr [10]>
-#   2 0.001            0.403     -1.20 <dbl [77,280]>  32029 <chr [10]>
-#   3 0.01             0.403     -1.31 <dbl [77,280]>  24025 <chr [10]>
-#   4 0.1              0.403     -1.24 <dbl [77,280]>  17804 <chr [10]>
-#   5 1                0.403     -1.25 <dbl [77,280]>  16860 <chr [10]>
+# # A tibble: 3 x 6
+#    alpha validation_loss intercept beta           nb_var message
+#    <dbl>           <dbl>     <dbl> <list>          <int> <list>
+# 1 0.0001           0.403     -1.09 <dbl [89,012]>  61609 <chr [10]>
+# 2 0.01             0.403     -1.16 <dbl [89,012]>  20564 <chr [10]>
+# 3 1                0.403     -1.10 <dbl [89,012]>  17094 <chr [10]>
 
 new_beta <- final_mod$beta.G
 
-length(ind <- which(new_beta != 0))  # 98,109
+length(ind <- which(new_beta != 0))  # 446,120
 summary(new_beta)
 #       Min.    1st Qu.     Median       Mean    3rd Qu.       Max.
-# -8.653e-02  0.000e+00  0.000e+00  1.676e-05  0.000e+00  1.172e-01
+# -8.224e-02  0.000e+00  0.000e+00  4.550e-06  0.000e+00  1.163e-01
 summary(new_beta[which(sign(new_beta * beta) < 0)])
 #       Min.    1st Qu.     Median       Mean    3rd Qu.       Max.
-# -2.981e-02 -2.703e-04 -1.818e-05 -6.502e-05  1.686e-04  3.635e-02
+# -2.551e-02 -8.117e-05 -4.700e-07 -2.134e-05  5.690e-05  4.051e-02
 
 pred <- final_mod$intercept +
   big_prodVec(G, new_beta[ind], ind.row = ind.test, ind.col = ind)
 
-AUCBoot(pred, y.sub[ind.test])  # 60.7 [60.0-61.4]
+AUCBoot(pred, y.sub[ind.test])  # 60.7 [60.0-61.3]
 
 
 library(tidyverse)
 
-ind2 <- sort(sample(ind, 10e3))
 ggplot(data.frame(y = new_beta, x = beta)[ind, ]) +
   geom_abline(slope = 1, intercept = 0, color = "red") +
   geom_abline(slope = 0, intercept = 0, color = "blue") +
@@ -189,8 +171,8 @@ std_prs <- grid2 %>%
   arrange(desc(auc)) %>%
   slice(1) %>%
   print()
-#   size thr.r2 thr.imp num   thr.lp      auc
-# 1  500    0.2     0.3  14 2.263405 0.566519
+#   size thr.r2 thr.imp num  thr.lp       auc
+# 1  500    0.2     0.3  14 2.30809 0.5667112
 
 ind.keep <- unlist(map(all_keep, std_prs$num))
 # Verif on training set
@@ -198,27 +180,27 @@ AUC(
   snp_PRS(G, beta[ind.keep], ind.test = ind.train, ind.keep = ind.keep,
           lpS.keep = lpval[ind.keep], thr.list = std_prs$thr.lp),
   y.sub[ind.train]
-)
+) # 0.5667112
 # Eval on test set
 AUCBoot(
   snp_PRS(G, beta[ind.keep], ind.test = ind.test, ind.keep = ind.keep,
           lpS.keep = lpval[ind.keep], thr.list = std_prs$thr.lp),
   y.sub[ind.test]
-) # 56.9 [56.2-57.6]
-sum(lpval[ind.keep] > std_prs$thr.lp)  # 3288
+) # 56.8 [56.2-57.5]
+sum(lpval[ind.keep] > std_prs$thr.lp)  # 3034
 
 max_prs <- grid2 %>% arrange(desc(auc)) %>% slice(1:10) %>% print() %>% slice(1)
 #    size thr.r2 thr.imp num   thr.lp       auc
-# 1  2500    0.2    0.90  72 3.645079 0.5703098
-# 2  2500    0.2    0.30  16 3.645079 0.5702892
-# 3  2500    0.2    0.60  44 3.645079 0.5702892
-# 4  2500    0.2    0.95 100 3.645079 0.5702163
-# 5  5000    0.1    0.95  96 4.176706 0.5698633
-# 6  2500    0.2    0.95 100 3.405206 0.5698004
-# 7  5000    0.1    0.95  96 4.470926 0.5697556
-# 8  2500    0.2    0.90  72 3.405206 0.5697458
-# 9  2500    0.2    0.95 100 3.901849 0.5697330
-# 10 5000    0.1    0.95  96 3.901849 0.5697202
+# 1  2500    0.2    0.90  72 3.667058 0.5699586
+# 2  2500    0.2    0.30  16 3.667058 0.5699415
+# 3  2500    0.2    0.60  44 3.667058 0.5699415
+# 4  2500    0.2    0.95 100 3.667058 0.5698932
+# 5  1000    0.5    0.90  76 1.748291 0.5698451
+# 6  1000    0.5    0.95 104 1.748291 0.5697478
+# 7  5000    0.1    0.95  96 4.022821 0.5696572
+# 8  2500    0.2    0.95 100 4.022821 0.5696496
+# 9  1000    0.5    0.60  48 1.748291 0.5696363
+# 10 1000    0.5    0.30  20 1.748291 0.5696320
 
 ind.keep <- unlist(map(all_keep, max_prs$num))
 AUCBoot(
@@ -226,12 +208,12 @@ AUCBoot(
           lpS.keep = lpval[ind.keep], thr.list = max_prs$thr.lp),
   y.sub[ind.test]
 ) # 57.3 [56.7-58.0]
-sum(lpval[ind.keep] > max_prs$thr.lp)  # 368
+sum(lpval[ind.keep] > max_prs$thr.lp)  # 360
 
 ggplot(grid2) +
   geom_point(aes(thr.lp, auc)) +
   facet_grid(thr.imp ~ thr.r2 + size) +
-  scale_x_log10(limits = c(1, 10), breaks = c(1, 2, 5), minor_breaks = 1:10) +
+  scale_x_log10(limits = c(0.3, 10), breaks = c(1, 2, 5), minor_breaks = 1:10) +
   ylim(0.54, NA) +
   theme_bigstatsr(size.rel = 0.7) +
   labs(x = "-log10(p-value) threshold (log scale)", y = "AUC")
